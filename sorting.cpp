84374e49f0d19882e771e0596e4aadf24eb411b4
Arghavan kassraie <111052412+Arghavankas2022@users.noreply.github.com>
2025-07-06 11:31:08 +0200

    added merge function

--- sorting.cpp
+++ sorting.cpp
@@ -21,19 +21,63 @@ public:
         return nums;
     }
     
-};
+    // Merge
+        std::vector<int> Merge(std::vector<int> X, std::vector<int> Y) {
+            int n = X.size();
+            int l = Y.size();
+            int x = 0, y = 0, z = 0;
+            
+            std::vector<int> Z(n + l);
+            
+            while ((x < n) && (y < l)) {
+                if (X[x] <= Y[y]) {
+                    Z[z] = X[x];
+                    x++;
+                } else {
+                    Z[z] = Y[y];
+                    y++;
+                }
+                z++;
+            }
 
-int main(){
-    
-    Solution Sol;
-    std::vector<int> array={3,5,6,2,7,4,3,19,3,22};
-    std::vector<int> sorted = Sol.SelectionSort(array);
+            while (x < n) {
+                Z[z] = X[x];
+                x++;
+                z++;
+            }
+
+            while (y < l) {
+                Z[z] = Y[y];
+                y++;
+                z++;
+            }
+
+            return Z;
+        }
+    };
+
+    int main(){
+        
+        Solution Sol;
+        std::vector<int> array={3,5,6,2,7,4,3,19,3,22};
+        std::vector<int> sorted = Sol.SelectionSort(array);
         std::cout << "Sorted array: ";
         for (int num : sorted) {
             std::cout << num << " ";
         }
-        std::cout << std::endl;}
-
+        std::cout << std::endl;
+        
+        std::vector<int> X = {3,4,2,5,6,7,23,1};
+        std::vector<int> Y = {4,5,6,31,2,3,8};
+        std::vector<int> merged = Sol.Merge(X,Y);
+        std::cout << "merged array: ";
+        for (int num : merged) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+        
+        
+    }
 
 
 

a8a3391cba17b7389a1ee955fa01e37eabe2ed8f
Arghavan kassraie <111052412+Arghavankas2022@users.noreply.github.com>
2025-10-29 22:27:05 +0100

    added comments

--- Geometric Algoirthms/Graham Scan/main.cpp
+++ Geometric Algoirthms/Graham Scan/main.cpp
@@ -1,3 +1,8 @@
+// Implementation of the Graham scan algorithm
+// sort all points , for the lower hull, start from the first two points, add third point if it doesnt form 
+// a right turn, keep adding points with the same logic, if a point is removed it may lead to a right turn in 
+// another point, hence that point should be removed too 
+// O(nlogn)
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -20,16 +25,17 @@ vector<Point> GrahamScan(vector<Point> &points){
     });
 
     vector<Point> hull;
+    // add first two points
     hull.push_back(points[0]);
     hull.push_back(points[1]);
     int n = points.size();
 
     // lower hull
     for (int i = 2; i < n; ++i) {
-        while (hull.size() >= 2 && right_turn(hull[hull.size()-2], hull[hull.size()-1], points[i])) {
-            hull.pop_back();
+        while (hull.size() >= 2 && right_turn(hull[hull.size()-2], hull[hull.size()-1], points[i])) { // compare each point with two previous points on the hull
+            hull.pop_back(); //remove if it makes a right turn
         }
-        hull.push_back(points[i]);
+        hull.push_back(points[i]); // if not, add to the hull
     }
 
     int h = hull.size();
@@ -48,7 +54,7 @@ vector<Point> GrahamScan(vector<Point> &points){
 }
 
 int main() {
-    // Hardcoded points
+    
     vector<Point> points = {
         {0, 0},
         {1, 2},
